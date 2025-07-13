8d19c1877f0dc9b47e66392ba6d00c59b44a3d3c
Serena <sha0720@naver.com>
2025-07-13 15:53:33 +0900

    chore: Main Stroyboard 삭제

--- JLife/JLife/Feature/Scene/CalendarScene/CalendarMain/CalendarMainView.swift
+++ JLife/JLife/Feature/Scene/CalendarScene/CalendarMain/CalendarMainView.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class CalendarMainView: UIView {
-//    private let
+    let navigationBar = ALNavigationBarView(left: .button(.coloredText(title: "Back", enabledColor: .blue, disabledColor: .brown)))
+    
+    init() {
+        super.init(frame: .zero)
+        
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
