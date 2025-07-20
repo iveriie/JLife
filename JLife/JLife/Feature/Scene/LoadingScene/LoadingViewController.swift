86af901a71536df206993cb32885b7ec06a82b3d
iveriie <owjddms1118@gmail.com>
2025-07-20 18:35:17 +0900

    [JL-5] feat: gif 재생 완료 시 RootView로 화면 전환 처리
    - LoadingViewController에서 isGifDone 알림 수신 시 popToRootViewController 실행

--- JLife/JLife/Feature/Scene/LoadingScene/LoadingViewController.swift
+++ JLife/JLife/Feature/Scene/LoadingScene/LoadingViewController.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
-
+  
   private let loadingView = LoadingView()
   
   override func loadView() {
     view = loadingView
   }
   
-    override func viewDidLoad() {
-    }
+  override func viewDidLoad() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(popToRootViewController),
+                                           name: NSNotification.Name("isGifDone"),
+                                           object: nil)
+  }
+  
+  // MARK: - Private Function
+  @objc
+  private func popToRootViewController() {
+    navigationController?.popToRootViewController(animated: true)
+  }
 }
 
 // MARK: - Preview
